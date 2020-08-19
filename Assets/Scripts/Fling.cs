900a499918193ffb536216b48b0318e23d2adafc
Jonah Warren <jonah@playfulsystems.com>
2020-08-19 14:18:08 -0400

    zipped package and scene rename

--- Assets/Scripts/Fling.cs
+++ Assets/Scripts/Fling.cs
@@ -6,29 +6,20 @@ using UnityEngine;
 public class Fling : MonoBehaviour
 {
     public float flingSpeed = 200f;
-
     Rigidbody2D rb;
     Vector2 mouseDownPos;
     Vector2 mouseUpPos;
 
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void OnMouseDown()
-    {
-        // get mouse position in world coordinates
-        mouseDownPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    }
-
-    void OnMouseUp()
-    {
+    void OnMouseUp() {
         // get mouse position in world coordinates
         mouseUpPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         // difference vector
-        Vector2 diff = mouseDownPos - mouseUpPos;
+        Vector2 diff = (Vector2)transform.position - mouseUpPos;
 
         // add that force with speed
         rb.AddForce(diff * flingSpeed);
